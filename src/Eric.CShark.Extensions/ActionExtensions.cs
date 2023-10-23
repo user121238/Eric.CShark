91c2984e88dc7cfca02565e34e8aa838af87d27f
卢勇富 <347655378@qq.com>
2023-10-23 15:47:32 +0800

    添加项目文件。

---
+++ src/Eric.CShark.Extensions/ActionExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Eric.CShark.Extensions
+{
+    public static class ActionExtensions
+    {
+	public static Action Debounce(this Action action, int delayMilliseconds)
+	{
+	    CancellationTokenSource? cts = null;
+
+	    void DebounceAction()
+	    {
+		    cts?.Cancel();
+		    cts = new CancellationTokenSource();
+		    Task.Delay(delayMilliseconds, cts.Token).ContinueWith(_ => action(), TaskContinuationOptions.OnlyOnRanToCompletion);
+	    }
+
+	    return DebounceAction;
+	}
+
+
+	public static Action Throttle(this Action action, int delayMilliseconds)
+	{
+	    bool isThrottled = false;
+
+	    void ThrottleAction()
+	    {
+		    if (!isThrottled)
+		    {
+			    action();
+			    isThrottled = true;
+			    Task.Delay(delayMilliseconds).ContinueWith(_ => isThrottled = false, TaskContinuationOptions.OnlyOnRanToCompletion);
+		    }
+	    }
+
+	    return ThrottleAction;
+	}
+    }
+}
---
+++ test/Eric.CShark.Utility.Tests/ActionHelperTest/ActionHelperTests.cs
@@ -0,0 +1,59 @@
+﻿using Eric.CShark.Utility;
+
+namespace Eric.CShark.Tests.ActionHelperTest
+{
+	[TestClass]
+	public class ActionHelperTests
+	{
+		private ActionHelper _actionHelper;
+
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_actionHelper = new ActionHelper();
+		}
+
+
+
+		[TestMethod]
+		public async Task TestDebounce()
+		{
+			
+			int execCount = 0;
+
+			Action action = () => execCount++;
+
+			_actionHelper.Debounce(action, 1000);
+			_actionHelper.Debounce(action, 1000);
+
+			await Task.Delay(3000);
+
+			Assert.AreEqual(execCount, 1);
+		}
+
+
+		[TestMethod]
+		public async Task TestThrottle()
+		{
+			int execCount = 0;
+
+			Action action = () => execCount++;
+
+			_actionHelper.Throttle(action,1000);
+			_actionHelper.Throttle(action,1000);
+
+			await Task.Delay(1500);
+
+			Assert.AreEqual(execCount, 1);
+
+			_actionHelper.Throttle(action, 500);
+
+			Assert.AreEqual(execCount, 2);
+
+		
+
+		}
+
+	}
+}
